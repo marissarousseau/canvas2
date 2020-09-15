12755a1d6c17477800692536db6fbbaa68fa5069
datdenkikniet <jcdra1@gmail.com>
2020-09-15 20:22:20 +0200

    Added "polyfill" for chrome, functionality is more or less the same on both chromium and firefox now

--- browser-action/background.js
+++ browser-action/background.js
@@ -18,7 +18,7 @@ function startBackgroundCheckingProcess() {
 function checkDarkModeEnabled(){
     getAll().then(result => {
         if (result.darkmode && !contentScriptContainer.contentScript) {
-            browser.contentScripts.register({
+            polyFillBrowser.contentScripts.register({
                 matches: ['https://*.instructure.com/*'],
                 css: [{file: '/content-scripts/canvas/main.css'}],
                 runAt: 'document_start',
--- common/common.js
+++ common/common.js
@@ -1,4 +1,4 @@
-const storage = browser.storage.local;
+const storage = polyFillBrowser.storage.local;
 const defaultBackgroundColor = '#000000';
 const defaultTextColor = '#ffffff';
 const defaultDarkMode = true;
@@ -39,21 +39,6 @@ async function getAll() {
 }
 
 /**
- * Storage works differently in chrome: it does not support Promises, so the result has to be "promistified". A polyfill
- * or some other approach will be needed to make this work seamlessly between chrome and firefox
- * @returns {Promise<CanvasDarkModeConfiguration>}
- */
-
-async function getAllChrome() {
-    return new Promise((resolve) => {
-        storage.get(keys, (values) => {
-            const configuration = extractConfiguration(values);
-            resolve(configuration);
-        });
-    });
-}
-
-/**
  * Set the stored values. If they are left out, no changes are made
  * @param bgColor A hex string describing the primary background color
  * @param txtColor A hex string describing the primary text color
@@ -67,7 +52,7 @@ function set(bgColor, txtColor, dm) {
     if (txtColor) {
         storage.set({textcolor: txtColor});
     }
-    if (dm) {
+    if (dm !== undefined) {
         storage.set({darkmode: dm});
     }
 }
